d7f76f3933879d5b8934358ebf5d9fe49ac701b6
deeapak Sarun <dyuvachandran@slu.edu>
2025-03-13 20:07:01 -0500

    final

--- app.py
+++ app.py
@@ -184,5 +184,10 @@ def process_frame():
 
 
 if __name__ == "__main__":
+    # Find an available port
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(("0.0.0.0", 0))  # Bind to an available port
+        s.listen(1)
+        port = s.getsockname()[1]  # Get the assigned port
 
-    app.run(host="0.0.0.0", port=4000, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
